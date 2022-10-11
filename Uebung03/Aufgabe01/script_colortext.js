4153d0f75d47a04569b8ef40fdf489dfaecb1722
FabianWaltisberg <fabian.waltisberg@gmail.com>
2022-10-11 16:47:39 +0200

    neue Version
    neue Version

--- Uebung03/Aufgabe01/script_colortext.js
+++ Uebung03/Aufgabe01/script_colortext.js
@@ -1,15 +1,17 @@
 "use strict";
 
-
 function changestyle() {
     var eingabefeld = document.querySelector("#src");
 
+    var a = eingabefeld.value   // Der Wert von der Eingabe vom Eingabefeld wird in a abgespeichert
+
         var random = Math.floor(Math.random()*16777215).toString(16);
-    var farbe = "#" + random;
+    var farbe = "#" + random;   // farbe ist eine random Farbe
 
-    eingabefeld.style["color"] = farbe;
-    eingabefeld.style["font-size"] = "30px";
-    eingabefeld.style["padding"] = "0px 0px";
-    eingabefeld.style["text-align"] = "center";
-
+    var o = document.querySelector("#ausgabe");
+    o.innerHTML = a;     // Der Text von a wird auf der Webseite bei unter Variable o in ausgabe angezeigt
+    o.style["color"] = farbe;
+    o.style["font-size"] = "30px";
+    o.style["padding"] = "0px 0px";
+    o.style["text-align"] = "center";   // div. Style für Text von ausgabe oder Variable a
 }
